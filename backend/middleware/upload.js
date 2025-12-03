270e2e6fd6d06acba091160590ca93ab41b7f38e
MoreLikeBean <61461251+snoob122@users.noreply.github.com>
2025-12-03 08:02:10 +0700

    cloudinary path fix

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary');
+const cloudinary = require('../utils/cloudinary');
 const path = require('path');
 
 // Configure storage
