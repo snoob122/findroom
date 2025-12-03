ee4a52a2ac94c812f966fdd5d3d2652ff1517b38
MoreLikeBean <61461251+snoob122@users.noreply.github.com>
2025-12-03 08:19:00 +0700

    added multer to backend

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const Listing = require('../models/Listing');
 const { auth, isLandlord } = require('../middleware/auth');
 //const upload = require('../middleware/upload');
